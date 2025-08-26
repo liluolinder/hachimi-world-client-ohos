fc3bcf886781239dcd2e122529cd9acaa53c8925
KonYaco <atwzj233@gmail.com>
2025-08-26 20:48:14 +0800

    [refactor] Extract Route from Navigator

--- composeApp/src/jvmMain/kotlin/world/hachimi/app/nav/Navigator.kt
+++ composeApp/src/jvmMain/kotlin/world/hachimi/app/nav/Navigator.kt
@@ -33,29 +33,3 @@ class Navigator(start: Any) {
         }
     }
 }
-
-sealed class Route {
-    sealed class Root : Route() {
-        companion object {
-            val Default = Home
-        }
-        data object Home: Root()
-        data object RecentPlay: Root()
-        data object RecentLike: Root()
-        data object MySubscribe: Root()
-        data object MyPlaylist: Root()
-        sealed class CreationCenter: Root() {
-            companion object Companion {
-                val Default = MyArtwork
-            }
-
-            object MyArtwork: CreationCenter()
-            object Publish: CreationCenter()
-        }
-        data object CommitteeCenter: Root()
-        data object ContributorCenter: Root()
-
-        data class Search(val query: String): Root()
-    }
-    data class Auth(val initialLogin: Boolean = true) : Route()
-}
