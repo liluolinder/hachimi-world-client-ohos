a9939b7c5724c3ec64edcdc8b5abcd6fb206d80d
KonYaco <atwzj233@gmail.com>
2025-08-27 17:33:43 +0800

    [feat](auth) Update UI

--- composeApp/src/jvmMain/kotlin/world/hachimi/app/ui/auth/AuthScreen.kt
+++ composeApp/src/jvmMain/kotlin/world/hachimi/app/ui/auth/AuthScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -28,125 +31,198 @@ fun AuthScreen(
     var isLogin by remember(displayLoginAsInitial) { mutableStateOf(displayLoginAsInitial) }
 
     Box(Modifier.fillMaxSize()) {
-        Column(Modifier.align(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(24.dp)) {
-            Text(text = if (isLogin) "欢迎回家" else "成为神人", style = MaterialTheme.typography.displayLarge)
-            Row {
-                if (isLogin) {
-                    Button(onClick = {}) {
-                        Text("登录")
-                    }
-                    TextButton(onClick = {
-                        isLogin = false
-                    }) {
-                        Text("注册")
-                    }
-                } else {
-                    TextButton(onClick = {
-                        isLogin = true
-                    }) {
-                        Text("登录")
-                    }
-                    Button(onClick = {}) {
-                        Text("注册")
+        Column(
+            modifier = Modifier.width(360.dp).align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            // Title
+            Text(
+                text = when {
+                    isLogin -> "欢迎回家"
+                    vm.regStep == 0 -> "成为神人"
+                    vm.regStep == 1 -> "离神人很近了"
+                    vm.regStep == 2 -> "只差一步"
+                    else -> error("unreachable")
+                },
+                style = MaterialTheme.typography.displayLarge
+            )
+
+            // Subtitle
+            if (!isLogin && vm.regStep != 0) Text(
+                text = when {
+                    vm.regStep == 1 -> "确认你的邮箱"
+                    vm.regStep == 2 -> "完善你的资料"
+                    else -> error("unreachable")
+                },
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            OutlinedCard(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Row(Modifier.padding(horizontal = 16.dp, vertical = 4.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                    if (isLogin) {
+                        Button(modifier = Modifier.weight(1f), onClick = {}) {
+                            Text("登录")
+                        }
+                        TextButton(modifier = Modifier.weight(1f), onClick = {
+                            isLogin = false
+                        }) {
+                            Text("注册")
+                        }
+                    } else {
+                        TextButton(modifier = Modifier.weight(1f), onClick = {
+                            isLogin = true
+                        }) {
+                            Text("登录")
+                        }
+                        Button(modifier = Modifier.weight(1f), onClick = {}) {
+                            Text("注册")
+                        }
                     }
                 }
             }
 
-            if (isLogin) {
-                TextField(vm.email, { vm.email = it }, placeholder = {
-                    Text("邮箱")
-                })
-                TextField(vm.password, { vm.password = it }, placeholder = {
-                    Text("密码")
-                }, visualTransformation = PasswordVisualTransformation())
-
-                Button(
-                    onClick = { vm.startLogin() },
-                    enabled = !vm.isOperating && vm.email.isNotBlank() && vm.password.isNotBlank(),
-                ) {
-                    Text("登录")
-                }
-            } else {
-                if (vm.regStep == 0) {
-                    TextField(vm.regEmail, { vm.regEmail = it }, placeholder = {
-                        Text("邮箱")
-                    }, singleLine = true)
-                    TextField(vm.regPassword, { vm.regPassword = it }, placeholder = {
-                        Text("密码")
-                    }, singleLine = true, visualTransformation = PasswordVisualTransformation())
-                    TextField(vm.regPasswordRepeat, { vm.regPasswordRepeat = it }, placeholder = {
-                        Text("确认密码")
-                    }, singleLine = true, visualTransformation = PasswordVisualTransformation())
-
-                    val enabled by derivedStateOf {
-                        vm.regEmail.isNotBlank() && vm.regPassword.isNotBlank()
-                                && vm.regPassword == vm.regPasswordRepeat
-                    }
-                    Button(
-                        onClick = { vm.regNextStep() },
-                        enabled = enabled && !vm.isOperating,
-                    ) {
-                        Text("下一步")
-                    }
-                } else if (vm.regStep == 1) {
-                    Text("离神人很近了")
-                    Text("确认你的邮箱")
-
-                    Text("已将验证码发送到您的邮箱")
-
-                    Row {
-                        TextField(vm.regCode, { vm.regCode = it }, placeholder = {
-                            Text("验证码")
-                        }, singleLine = true)
-                        val sendCodeEnabled = vm.regCodeRemainSecs < 0
+            OutlinedCard(modifier = Modifier.fillMaxWidth()) {
+                Column(Modifier.padding(24.dp), verticalArrangement = Arrangement.spacedBy(24.dp)) {
+                    if (isLogin) {
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = vm.email,
+                            onValueChange = { vm.email = it },
+                            label = { Text("邮箱") },
+                            singleLine = true
+                        )
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = vm.password,
+                            onValueChange = { vm.password = it },
+                            label = { Text("密码") },
+                            singleLine = true,
+                            visualTransformation = PasswordVisualTransformation()
+                        )
 
                         Button(
-                            onClick = { vm.regSendEmailCode() },
-                            enabled = sendCodeEnabled && !vm.isOperating
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = { vm.startLogin() },
+                            enabled = !vm.isOperating && vm.email.isNotBlank() && vm.password.isNotBlank(),
                         ) {
-                            Text(
-                                if (sendCodeEnabled) "重新发送"
-                                else "重新发送 (${vm.regCodeRemainSecs} 秒)"
-                            )
+                            Text("登录")
                         }
-                    }
-                    Button(
-                        onClick = { vm.regNextStep() },
-                        enabled = vm.regCode.isNotBlank() && !vm.isOperating
-                    ) {
-                        Text("下一步")
-                    }
-                } else if (vm.regStep == 2) {
-                    Text("只差一步")
-                    Text("完善你的资料")
+                    } else {
+                        if (vm.regStep == 0) {
+                            TextField(
+                                modifier = Modifier.fillMaxWidth(),
+                                value = vm.regEmail,
+                                onValueChange = { vm.regEmail = it },
+                                label = { Text("邮箱") },
+                                singleLine = true
+                            )
+                            TextField(
+                                modifier = Modifier.fillMaxWidth(),
+                                value = vm.regPassword,
+                                onValueChange = { vm.regPassword = it },
+                                label = { Text("密码") },
+                                singleLine = true,
+                                visualTransformation = PasswordVisualTransformation()
+                            )
+                            TextField(
+                                modifier = Modifier.fillMaxWidth(),
+                                value = vm.regPasswordRepeat,
+                                onValueChange = { vm.regPasswordRepeat = it },
+                                label = { Text("确认密码") },
+                                singleLine = true,
+                                visualTransformation = PasswordVisualTransformation()
+                            )
 
+                            val enabled by derivedStateOf {
+                                vm.regEmail.isNotBlank() && vm.regPassword.isNotBlank()
+                                        && vm.regPassword == vm.regPasswordRepeat
+                            }
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = { vm.regNextStep() },
+                                enabled = enabled && !vm.isOperating,
+                            ) {
+                                Text("下一步")
+                            }
+                        } else if (vm.regStep == 1) {
+                            Text("已将验证码发送到您的邮箱", style = MaterialTheme.typography.labelMedium)
 
-                    TextField(vm.name, { vm.name = it }, placeholder = {
-                        Text("昵称")
-                    }, singleLine = true)
-                    TextField(vm.intro, { vm.intro = it }, placeholder = {
-                        Text("介绍一下")
-                    })
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(24.dp)
+                            ) {
+                                TextField(
+                                    modifier = Modifier.weight(1f),
+                                    value = vm.regCode,
+                                    onValueChange = { vm.regCode = it },
+                                    label = { Text("验证码") },
+                                    singleLine = true
+                                )
+                                val sendCodeEnabled = vm.regCodeRemainSecs < 0
 
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        RadioButton(selected = vm.gender == 0, onClick = { vm.gender = 0 })
-                        Text("男")
+                                Button(
+                                    onClick = { vm.regSendEmailCode() },
+                                    enabled = sendCodeEnabled && !vm.isOperating
+                                ) {
+                                    Text(
+                                        if (sendCodeEnabled) "重新发送"
+                                        else "重新发送 (${vm.regCodeRemainSecs} 秒)"
+                                    )
+                                }
+                            }
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = { vm.regNextStep() },
+                                enabled = vm.regCode.isNotBlank() && !vm.isOperating
+                            ) {
+                                Text("下一步")
+                            }
+                        } else if (vm.regStep == 2) {
+                            TextField(
+                                modifier = Modifier.fillMaxWidth(),
+                                value = vm.name,
+                                onValueChange = { vm.name = it },
+                                label = { Text("昵称") },
+                                singleLine = true
+                            )
+                            TextField(
+                                modifier = Modifier.fillMaxWidth(),
+                                value = vm.intro,
+                                onValueChange = { vm.intro = it },
+                                label = { Text("介绍一下") },
+                                maxLines = 4,
+                                minLines = 4
+                            )
 
-                        RadioButton(selected = vm.gender == 1, onClick = { vm.gender = 1 })
-                        Text("女")
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                RadioButton(selected = vm.gender == 0, onClick = { vm.gender = 0 })
+                                Text("男")
 
-                        RadioButton(selected = vm.gender == 2, onClick = { vm.gender = 2 })
-                        Text("神没有性别")
-                    }
-                    Button(
-                        onClick = { vm.finishRegister() },
-                        enabled = vm.name.isNotBlank() && vm.intro.isNotBlank() && vm.gender != null && !vm.isOperating
-                    ) {
-                        Text("完成")
-                    }
+                                RadioButton(selected = vm.gender == 1, onClick = { vm.gender = 1 })
+                                Text("女")
 
-                    Button(onClick = { vm.skipProfile() }) {
-                        Text("跳过")
+                                RadioButton(selected = vm.gender == 2, onClick = { vm.gender = 2 })
+                                Text("神没有性别")
+                            }
+
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = { vm.finishRegister() },
+                                enabled = vm.name.isNotBlank() && vm.intro.isNotBlank() && vm.gender != null && !vm.isOperating
+                            ) {
+                                Text("完成")
+                            }
+
+                            TextButton(
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = { vm.skipProfile() }
+                            ) {
+                                Text("跳过")
+                            }
+                        }
                     }
                 }
             }
