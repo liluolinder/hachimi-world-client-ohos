eca3314d1a7310173f0bd9fb52efdb9bd9263474
KonYaco <atwzj233@gmail.com>
2025-08-31 01:57:31 +0800

    [fix](ui) Initialize at startup

--- composeApp/src/androidMain/kotlin/world/hachimi/app/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/world/hachimi/app/MyApplication.kt
@@ -1,9 +1,11 @@
 package world.hachimi.app
 
 import android.app.Application
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import world.hachimi.app.di.appModule
+import world.hachimi.app.model.GlobalStore
 
 class MyApplication: Application() {
     override fun onCreate() {
@@ -15,5 +17,8 @@ class MyApplication: Application() {
             androidContext(this@MyApplication)
             modules(appModule)
         }
+
+        val global = inject<GlobalStore>()
+        global.value.initialize()
     }
 }
