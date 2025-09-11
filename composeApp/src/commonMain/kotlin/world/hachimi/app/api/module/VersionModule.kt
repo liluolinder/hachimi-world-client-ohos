080278fd1a863a091f21bd7330a80a687110e14f
KonYaco <atwzj233@gmail.com>
2025-09-11 20:17:03 +0800

    [feat](api) Add `/version/latest` api

--- composeApp/src/commonMain/kotlin/world/hachimi/app/api/module/VersionModule.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/api/module/VersionModule.kt
@@ -1,5 +1,6 @@
 package world.hachimi.app.api.module
 
+import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
 import world.hachimi.app.api.ApiClient
 import world.hachimi.app.api.WebResult
@@ -15,4 +16,22 @@ class VersionModule(
 
     suspend fun server(): WebResult<ServerVersion> =
         client.get("/version/server")
+
+    @Serializable
+    data class LatestVersionReq (
+        val variant : String
+    )
+
+    @Serializable
+    data class LatestVersionResp (
+        val versionName: String,
+        val versionNumber: Int,
+        val changelog: String,
+        val variant: String,
+        val url: String,
+        val releaseTime : Instant
+    )
+
+    suspend fun latest(req: LatestVersionReq): WebResult<LatestVersionResp?> =
+        client.get("/version/latest", req)
 }
