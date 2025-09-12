94ee472e297295a735c84b84644831a67719e83c
KonYaco <atwzj233@gmail.com>
2025-09-12 16:30:20 +0800

    [feat](kmp) Update di module definition

--- composeApp/src/wasmJsMain/kotlin/world/hachimi/app/di/di.kt
+++ composeApp/src/wasmJsMain/kotlin/world/hachimi/app/di/di.kt
@@ -1,41 +1,36 @@
 package world.hachimi.app.di
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
-import okio.Path.Companion.toOkioPath
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import world.hachimi.app.BuildKonfig
-import world.hachimi.app.JVMPlatform
 import world.hachimi.app.api.ApiClient
 import world.hachimi.app.model.AuthViewModel
-import world.hachimi.app.model.PublishViewModel
 import world.hachimi.app.model.GlobalStore
 import world.hachimi.app.model.MainViewModel
 import world.hachimi.app.model.MyArtworkViewModel
 import world.hachimi.app.model.PlaylistDetailViewModel
 import world.hachimi.app.model.PlaylistViewModel
+import world.hachimi.app.model.PublishViewModel
 import world.hachimi.app.model.RecentPlayViewModel
 import world.hachimi.app.model.ReviewDetailViewModel
 import world.hachimi.app.model.ReviewViewModel
 import world.hachimi.app.model.SearchViewModel
 import world.hachimi.app.model.UserSpaceViewModel
 import world.hachimi.app.player.Player
-import world.hachimi.app.player.JVMPlayer
+import world.hachimi.app.player.WasmPlayer
 import world.hachimi.app.storage.MyDataStore
 import world.hachimi.app.storage.MyDataStoreImpl
 import world.hachimi.app.storage.SongCache
 import world.hachimi.app.storage.SongCacheImpl
 
 val appModule = module {
-    single { ApiClient(BuildKonfig.API_BASE_URL) }
-    single { getPreferencesDataStore() }
-    single<MyDataStore> { MyDataStoreImpl(get()) }
-    single<Player> { JVMPlayer() }
+    single {
+        ApiClient(BuildKonfig.API_BASE_URL)
+    }
+    single<MyDataStore> { MyDataStoreImpl() }
+    single<Player> { WasmPlayer() }
     single<SongCache> { SongCacheImpl() }
-
     singleOf(::GlobalStore)
 
     viewModelOf(::MainViewModel)
@@ -49,10 +44,4 @@ val appModule = module {
     viewModelOf(::RecentPlayViewModel)
     viewModelOf(::ReviewViewModel)
     viewModelOf(::ReviewDetailViewModel)
-}
-
-private fun getPreferencesDataStore(): DataStore<Preferences> {
-    val file = JVMPlatform.getDataDir().file.resolve("settings.preferences_pb")
-
-    return PreferenceDataStoreFactory.createWithPath { file.toOkioPath() }
 }
