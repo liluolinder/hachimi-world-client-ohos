1e17abdea33488ca5fb46e7e11d88e87b2c1f74e
KonYaco <atwzj233@gmail.com>
2025-09-12 16:30:30 +0800

    [feat](kmp) Start koin in wasm target

--- composeApp/src/wasmJsMain/kotlin/world/hachimi/app/main.kt
+++ composeApp/src/wasmJsMain/kotlin/world/hachimi/app/main.kt
@@ -4,11 +4,17 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.koin.core.context.startKoin
+import world.hachimi.app.di.appModule
 import world.hachimi.app.font.WithFont
 import world.hachimi.app.ui.App
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    startKoin {
+        modules(appModule)
+    }
+
     ComposeViewport(document.body!!) {
         LaunchedEffect(Unit) {
             document.querySelector("#loading")?.remove()
