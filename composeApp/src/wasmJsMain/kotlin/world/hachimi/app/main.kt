99c7cdb28bf2f35a1f6400eb06eb5ac5f4dc950d
KonYaco <atwzj233@gmail.com>
2025-09-27 14:30:06 +0800

    [feat](wasm)(ui) Synchronize navigation state with browser history and handle back navigation

--- composeApp/src/wasmJsMain/kotlin/world/hachimi/app/main.kt
+++ composeApp/src/wasmJsMain/kotlin/world/hachimi/app/main.kt
@@ -2,17 +2,19 @@ package world.hachimi.app
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.backhandler.BackHandler
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.browser.window
 import org.koin.core.context.startKoin
 import world.hachimi.app.di.appModule
 import world.hachimi.app.font.WithFont
 import world.hachimi.app.model.GlobalStore
 import world.hachimi.app.ui.App
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalWasmJsInterop::class)
 fun main() {
     val koin = startKoin {
         modules(appModule)
@@ -21,14 +23,34 @@ fun main() {
     val global = koin.koin.get<GlobalStore>()
     global.initialize()
 
+    window.addEventListener("popstate") {
+        it.preventDefault()
+        global.nav.back()
+    }
+
+    if (window.location.hash.isEmpty()) {
+        window.history.pushState(null, "", "#/")
+    }
+
+    val previousBackStack = mutableStateOf(global.nav.backStack.toList())
+    val nav = global.nav
     ComposeViewport(document.body!!) {
         LaunchedEffect(Unit) {
             document.querySelector("#loading")?.remove()
         }
-        BackHandler {
-            if (global.nav.backStack.size > 1) {
-                global.nav.back()
-            }
+        LaunchedEffect(Unit) {
+            snapshotFlow { nav.backStack.toList() }
+                .collect { newBackStack ->
+                    val lastEntry = newBackStack.last()
+                    if (previousBackStack.value.size < newBackStack.size) {
+                        window.history.pushState(lastEntry.toString().toJsString(), "", "#/${lastEntry}")
+                    } else if (previousBackStack.value.size == newBackStack.size) {
+                        window.history.replaceState(lastEntry.toString().toJsString(), "", "#/${lastEntry}")
+                    } else if (previousBackStack.value.size > newBackStack.size) {
+
+                    }
+                    previousBackStack.value = newBackStack
+                }
         }
         WithFont {
             if (global.initialized) {
