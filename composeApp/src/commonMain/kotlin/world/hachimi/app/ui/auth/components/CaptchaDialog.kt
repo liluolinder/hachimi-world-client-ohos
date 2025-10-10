b0bb7940fdc310ba9640d823a7cc4d71b7a92a98
KonYaco <atwzj233@gmail.com>
2025-10-10 19:39:32 +0800

    [feat](captcha) Add guide

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/auth/components/CaptchaDialog.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/auth/components/CaptchaDialog.kt
@@ -14,7 +14,7 @@ fun CaptchaDialog(onConfirm: () -> Unit) {
             Text("请完成人机验证")
         },
         text = {
-            Text("请在打开的浏览器页面中完成人机验证")
+            Text("请在打开的浏览器页面中完成人机验证。\n若未正确打开，请检查浏览器安全设置中是否启用了阻止弹出式窗口选项。")
         },
         confirmButton = {
             TextButton(onClick = onConfirm) {
